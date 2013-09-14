575bc1db776525617e24d8dbe509be78dd7da9c6
Dave <davidsimpson1@MacMini.local>
2013-09-14 16:13:19 +1000

    few comments

--- flaskr.py
+++ flaskr.py
@@ -4,6 +4,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
 
+# few dummy comments for commit
 
 # configuration
 DATABASE = '/Users/davesimpson/PycharmProjects/flaskr/flaskr.db'
