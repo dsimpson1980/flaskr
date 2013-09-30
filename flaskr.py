4b85f57f659bcd77acff9b7745cba0ab5f931162
David Simpson <mapdes@gmail.com>
2013-09-30 21:20:49 +1000

    Removed redundant comments

--- flaskr.py
+++ flaskr.py
@@ -11,8 +11,6 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#Comment added on shiny thing
-
 # postgres config
 SQLALCHEMY_DATABASE_URI = "postgresql://mapdes:default@localhost/flaskr"
 SQLALCHEMY_ECHO = True
@@ -23,53 +21,32 @@ DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
-# engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
 
-# create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-# #db = SQLAlchemy(app)
 engine = create_engine(SQLALCHEMY_DATABASE_URI, convert_unicode=True)
 
-# db_session = scoped_session(sessionmaker(autocommit=False,
-#                                          autoflush=False,
-#                                          bind=engine))
-# Base = declarative_base()
-# Base.query = db_session.query_property()
-#
 def connect_db():
     return engine.connect()
-#     sqlite approach
-#     return sqlite3.connect(app.config['DATABASE'])
-#
+
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.execute(f.read())
         db.commit()
-#
-# @app.teardown_appcontext
-# def shutdown_session(exception=None):
-#     db_session.remove()
 
 @app.before_request
 def before_request():
-    # g and sqlite approach
     g.db = connect_db()
 
 @app.teardown_request
 def teardown_request(exception):
-    # g and sqlite approach
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
 
 @app.route('/')
 def show_customers():
-    # con = engine.connect()
-    # cur = con.execute('SELECT customer_id, name, market FROM customers')
-    # customers = [dict(customer_id=row[0], name=row[1], market=row[2]) for row in cur.fetchall()]
-    # return render_template('show_customers.html', customers=customers)
     cur = g.db.execute('SELECT customer_id, name, market FROM retail.customers')
     customers = [dict(customer_id=row[0], name=row[1], market=row[2]) for row in cur.fetchall()]
     return render_template('show_customers.html', customers=customers)
@@ -79,14 +56,6 @@ def add_customer():
     from StringIO import StringIO
     if not session.get('logged_in'):
         abort(401)
-    # con = engine.connect()
-    # con.execute('INSERT INTO retail.customers (name, market) VALUES (?, ?)',
-    #              [request.form['name'], request.form['market']])
-    # con.commit()
-    # demand = generate_random_customer_data()
-    # add push to db demand table here
-    # flash('New customer was successfully added')
-    # return redirect(url_for('show_customers'))
     demand = generate_random_customer_data()
     image64 = generate_customer_demand_image(demand)
 
@@ -96,7 +65,6 @@ def add_customer():
                                [request.form['name'],
                                 request.form['market'],
                                 image64]).first()[0]
-    # g.db.commit()
     ids = np.array(range(len(demand)))
     ids.fill(customer_id)
     demand_data = pd.DataFrame({'customer_id': ids,
@@ -117,14 +85,17 @@ def add_customer():
 def display_customer(customer_id):
     if not session.get('logged_in'):
         abort(401)
-    # return 'customer_id = ' + str(customer_id)
-    cur = g.db.execute('SELECT customer_id, name, market, image64 FROM retail.customers WHERE customer_id = %s' % customer_id)
+    cur = g.db.execute('''SELECT customer_id, name, market, image64
+                          FROM retail.customers
+                          WHERE customer_id = %s''' % customer_id)
     row = cur.fetchall()
     customer_meta_data = dict(customer_id=row[0][0],
                                name=row[0][1],
                                market=row[0][2],
                                image64=row[0][3])
-    cur = g.db.execute('SELECT datetime, value FROM retail.customer_demand WHERE customer_id =' + str(customer_id))
+    cur = g.db.execute('''SELECT datetime, value
+                          FROM retail.customer_demand
+                          WHERE customer_id =''' + str(customer_id))
     recordset = cur.fetchall()
     if recordset != []:
         dates, values = zip(*recordset)
@@ -144,9 +115,9 @@ def display_customer(customer_id):
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into retail.entries (title, text) values (%s, %s)',
+    g.db.execute('''INSERT INTO retail.entries (title, text)
+                    VALUES (%s, %s)''',
                  [request.form['title'], request.form['text']])
-    # g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
