653b973a7f4b8aa38b5a5815fd7c8de0841e4ac8
David Simpson <mapdes@gmail.com>
2013-10-09 16:52:13 +1100

    Wrote schema.sql for postgres database

--- schema.sql
+++ schema.sql
@@ -1,33 +1,51 @@
-drop table if exists entries;
-drop table if exists retail.demand;
-drop table if exists retail.customers;
-drop sequence if exists retail.customer_ids_sequence;
-drop table if exists retail.markets;
+DROP TABLE IF EXISTS retail.premiums;
+DROP SEQUENCE IF EXISTS retail.premium_ids_sequence;
+DROP TABLE IF EXISTS retail.run_parameters;
+DROP SEQUENCE IF EXISTS retail.parameter_ids_sequence;
+DROP TABLE IF EXISTS retail.customer_demand;
+DROP TABLE IF EXISTS retail.customers;
+DROP SEQUENCE IF EXISTS retail.customer_ids_sequence;
+DROP TABLE IF EXISTS retail.markets;
 
-create sequence entries_sequence;
-create table entries (
-  id integer primary key default nextval(entries_sequence) not null,
-  title text not null,
-  text text not null
+CREATE TABLE retail.markets
+(
+  market CHAR(15) NOT NULL PRIMARY KEY,
+  country_code CHAR(2) NOT NULL,
+  commodity CHAR(10) NOT NULL
 );
 
-create table retail.markets (
-  market char(15) primary key,
-  country_code char(2) not null,
-  commodity char(10) not null
+CREATE SEQUENCE retail.customer_ids_sequence;
+CREATE TABLE retail.customers
+(
+  customer_id integer NOT NULL PRIMARY KEY DEFAULT NEXTVAL('retail.customer_ids_sequence'),
+  name CHAR(15) NOT NULL,
+  market CHAR(15) NOT NULL REFERENCES retail.markets(market),
+  image64 bytea
 );
 
-create sequence retail.customer_ids_sequence;
-create table retail.customers (
-  customer_id integer primary key default not null,
-  name varchar not null,
-  market char(15) not null references retail.markets(market)
+CREATE TABLE retail.customer_demand
+(
+  customer_id INTEGER NOT NULL REFERENCES retail.customers(customer_id),
+  datetime TIMESTAMP NOT NULL,
+  value FLOAT(19) NOT NULL,
+  CONSTRAINT customer_demand_pkey PRIMARY KEY (customer_id, datetime)
 );
 
-create table retail.demand (
-  customer_id integer not null references retail.customers(customer_id),
-  datetime_utc timestamp not null,
-  value float not null,
-  primary key(customer_id, datetime_utc)
+CREATE SEQUENCE retail.parameter_ids_sequence;
+CREATE TABLE retail.run_parameters
+(
+  run_id integer NOT NULL PRIMARY KEY DEFAULT NEXTVAL('retail.parameter_ids_sequence'),
+  parameters bytea NOT NULL,
+  db_upload_datetime TIMESTAMP NOT NULL
 );
 
+CREATE SEQUENCE retail.premium_ids_sequence;
+CREATE TABLE retail.premiums
+(
+  premium_id integer NOT NULL PRIMARY KEY DEFAULT NEXTVAL('retail.premium_ids_sequence'),
+  run_id integer NOT NULL REFERENCES retail.run_parameters(run_id),
+  customer_id integer NOT NULL REFERENCES retail.customers(customer_id),
+  contract_start_date_utc TIMESTAMP NOT NULL,
+  contract_end_date_utc TIMESTAMP NOT NULL,
+  premium FLOAT NOT NULL
+);
