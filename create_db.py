3241a4566e65e84ad156493ede0b3adb4e35ff23
David Simpson <mapdes@gmail.com>
2013-10-17 17:54:33 +1100

    Converted module to package

--- create_db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from config import SQLALCHEMY_DATABASE_URI
-
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=False)
-
-with engine.connect() as conn:
-    conn.execute(open('schema.sql', 'r').read())
--- runp-heroku.py
+++
@@ -1,2 +0,0 @@
-#!flask/bin/python
-from flaskr import app
