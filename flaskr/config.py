5d83bf3af3a2aa52fee83cd6b5211934949aa260
David Simpson <mapdes@gmail.com>
2013-10-19 19:49:46 +1100

    Config celery for heroku

--- flaskr/config.py
+++ flaskr/config.py
@@ -2,12 +2,12 @@ import os
 
 if os.environ.get('HEROKU') is None:
     SQLALCHEMY_DATABASE_URI = 'postgresql://mapdes:default@localhost/flaskr'
-    CELERY_BROKER_URI = 'amqp://localhost:5672',
-    CELERY_RESULT_BACKEND = CELERY_BROKER_URI
+    CELERY_BROKER_URL = 'amqp://localhost:5672',
+    CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    CELERY_BROKER_URI = os.environ['CLOUDAMQP_URL']
-    CELERY_RESULT_BACKEND = CELERY_BROKER_URI
+    CELERY_BROKER_URL = os.environ['CLOUDAMQP_URL']
+    CELERY_RESULT_BACKEND = CELERY_BROKER_URL
     BROKER_POOL_LIMIT = 1
 
 SQLALCHEMY_ECHO = True
