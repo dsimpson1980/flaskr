7639401b67a32f62f3fd841c04743127b59ae9dc
David Simpson <mapdes@gmail.com>
2013-10-19 19:42:30 +1100

    Added heroku default AMQP variables

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -15,15 +15,10 @@ meta.reflect(bind=engine, schema=schema)
 db = SQLAlchemy(app)
 
 app = Flask(__name__)
-app.config.update(
-    #CELERY_BROKER_URL='redis://localhost:6379',
-    #CELERY_RESULT_BACKEND='redis://localhost:6379'
-    CELERY_BROKER_URL='amqp://localhost:5672',
-    CELERY_RESULT_BACKEND='amqp://localhost:5672'
-)
 
 def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL',
+                                                       'BROKER_POOL_LIMIT'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
     class ContextTask(TaskBase):
